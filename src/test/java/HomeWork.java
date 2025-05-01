e0ad6698de6f230f491816f7abce49c3740d1b27
ViLevin <vilevinqa@gmail.com>
2025-05-01 14:31:22 +0300

    ClassWork1

---
+++ src/test/java/HomeWork.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class HomeWork {
+    WebDriver wd;
+
+    @BeforeClass
+    public void setUp() {
+        wd = new ChromeDriver();
+        wd.get("https://telranedu.web.app/login");
+
+
+    }
+
+
+    @Test
+    public void cssSelection() {
+        //by tag name
+
+        // by class
+
+        //by id
+
+        //by attribute
+
+    }
+}
---
+++ src/test/java/LoginTests.java
@@ -0,0 +1,55 @@
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginTests {
+
+    @BeforeClass
+    public void preCondition() {
+
+        //open browser
+        //open site
+
+    }
+
+    @Test
+    public void loginSuccess() {
+        //open form (find element + click)
+        //fill form (find element + click + clear + type)
+        //submit form (find element + click)
+
+    }
+
+    @Test
+    public void loginWrongEmail() {
+//open form (find element + click)
+        //fill form (find element + click + clear + type)
+        //submit form (find element + click)
+
+    }
+
+    @Test
+    public void loginWrongPassword() {
+        //open form (find element + click)
+        //fill form (find element + click + clear + type)
+        //submit form (find element + click)
+
+    }
+
+    @Test
+    public void loginUnregistered() {
+
+        //open form
+        //fill form
+        //submit form
+
+    }
+
+    @AfterClass
+    public void postCondition() {
+        // close browser
+
+    }
+
+
+}
